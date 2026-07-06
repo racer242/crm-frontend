7bcba85a8c98db10082c289c03c7c116d759e325
Racer242 <racer242@gmail.com>
2026-07-06 16:03:12 +0300

    feat: добавлены request-адаптеры для prize.get и receipt.get

---
+++ config/adapters/prize.get.request.js
@@ -0,0 +1,7 @@
+/**
+ * Пустой request-адаптер для prize.get
+ * API не требует дополнительных параметров
+ */
+function transform(params = {}) {
+  return {};
+}
---
+++ config/adapters/receipt.get.request.js
@@ -0,0 +1,7 @@
+/**
+ * Пустой request-адаптер для receipt.get
+ * API не требует дополнительных параметров
+ */
+function transform(params = {}) {
+  return {};
+}
